49d521efb6ec452064f815043e43fdcbafc87f33
rBonilla <rBonilla@DRML17038.daiglobal.net>
2020-10-12 20:04:53 -0600

    Este el el commit inicial

---
+++ src/main/java/net/cuscatlan/sfcrecipeapp/SfcRecipeAppApplication.java
@@ -0,0 +1,13 @@
+package net.cuscatlan.sfcrecipeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfcRecipeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfcRecipeAppApplication.class, args);
+	}
+
+}
